e7324c44051a47bcc510063c973a99528f59dc41
Blazej Bucko <blazej.bucko@gmail.com>
2014-11-24 09:24:53 +0100

    Added HTTP

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
+func getEnv(key string, defVal string) (env string) {
+	env = os.Getenv(key)
+
+	if env == "" {
+		env = defVal
+	}
+	return
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
+
+func main() {
+	host := getEnv("HOST", "")
+	port := getEnv("PORT", "8080")
+
+	bind := fmt.Sprintf("%s:%s", host, port)
+	log.Println("Starting server on", bind)
+
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(bind, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
