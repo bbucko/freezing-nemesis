da6aaa2f98728e84aae44515677fa66e62e0a842
Blazej Bucko <blazej.bucko@gmail.com>
2014-11-25 10:46:44 +0100

    Added NewRelic and Negroni

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/bbucko/freezing-nemesis/heroku"
+	"github.com/codegangsta/negroni"
+	negronigorelic "github.com/jingweno/negroni-gorelic"
 	"github.com/yvasiyarov/gorelic"
 	"gopkg.in/mgo.v2"
 	"log"
@@ -17,11 +19,10 @@ func main() {
 	host := heroku.GetEnv("HOST", "")
 	port := heroku.GetEnv("PORT", "8080")
 	mongoHost := heroku.GetEnv("MONGOHQ_URL", "localhost:27017")
-	newRelic := heroku.GetEnv("NEW_RELIC_LICENSE_KEY", "")
+	newRelicLicenseKey := heroku.GetEnv("NEW_RELIC_LICENSE_KEY", "")
 
 	agent := gorelic.NewAgent()
-	agent.Verbose = true
-	agent.NewrelicLicense = newRelic
+	agent.NewrelicLicense = newRelicLicenseKey
 	agent.Run()
 
 	log.Println("Connecting to mongo: ", mongoHost)
@@ -35,10 +36,16 @@ func main() {
 	bind := fmt.Sprintf("%s:%s", host, port)
 	log.Println("Starting server on", bind)
 
-	http.HandleFunc("/", handler)
-	httpErr := http.ListenAndServe(bind, nil)
-	if httpErr != nil {
-		log.Fatal("ListenAndServe: ", httpErr)
-	}
-	fmt.Println("hello world")
+	r := http.NewServeMux()
+	r.HandleFunc(`/`, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "success!\n")
+	})
+
+	n := negroni.New()
+
+	n.Use(negronigorelic.New(newRelicLicenseKey, "freezing-nemesis", false))
+	n.UseHandler(r)
+
+	n.Run(bind)
 }
