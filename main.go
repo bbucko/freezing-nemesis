1e7dbcad3e2bf48d4555bc87efce7e5fa4fb1bf5
Blazej Bucko <blazej.bucko@gmail.com>
2014-11-25 10:37:22 +0100

    Added NewRelic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/bbucko/freezing-nemesis/heroku"
+	"github.com/yvasiyarov/gorelic"
 	"gopkg.in/mgo.v2"
 	"log"
 	"net/http"
@@ -16,12 +17,18 @@ func main() {
 	host := heroku.GetEnv("HOST", "")
 	port := heroku.GetEnv("PORT", "8080")
 	mongoHost := heroku.GetEnv("MONGOHQ_URL", "localhost:27017")
+	newRelic := heroku.GetEnv("NEW_RELIC_LICENSE_KEY", "")
+
+	agent := gorelic.NewAgent()
+	agent.Verbose = true
+	agent.NewrelicLicense = newRelic
+	agent.Run()
+
 	log.Println("Connecting to mongo: ", mongoHost)
+	session, mongoErr := mgo.Dial(mongoHost)
 
-	session, err := mgo.Dial(mongoHost)
-
-	if err != nil {
-		log.Fatal("Mongo: ", err)
+	if mongoErr != nil {
+		log.Fatal("Mongo: ", mongoErr)
 	}
 	defer session.Close()
 
@@ -29,8 +36,9 @@ func main() {
 	log.Println("Starting server on", bind)
 
 	http.HandleFunc("/", handler)
-	err = http.ListenAndServe(bind, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	httpErr := http.ListenAndServe(bind, nil)
+	if httpErr != nil {
+		log.Fatal("ListenAndServe: ", httpErr)
 	}
+	fmt.Println("hello world")
 }
